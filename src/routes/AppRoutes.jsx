0316a2109c04c17e609282a0255848c7cb3de9a1
itskotcha <kotchawq@gmail.com>
2026-02-17 13:14:32 +0700

    app routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../components/auth/Login";
-import Register from "../components/auth/Register"; // 1. เพิ่มการ import
+import Register from "../components/auth/Register";
 import Dashboard from "../components/admin/Dashboard";
 import RoomList from "../components/user/RoomList";
 import MyBookings from "../components/user/MyBookings";
+
+// 1. Import the missing Admin components
+import ManageRooms from "../components/admin/ManageRooms";
+import ManageBookings from "../components/admin/ManageBookings";
+import ManageUsers from "../components/admin/ManageUsers";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} /> {/* 2. เพิ่ม Route นี้ */}
+      <Route path="/register" element={<Register />} />
       
       {/* ส่วนของ User */}
       <Route path="/user" element={<RoomList />} />
@@ -17,6 +22,11 @@ export default function AppRoutes() {
 
       {/* ส่วนของ Admin */}
       <Route path="/admin" element={<Dashboard />} />
+      
+      {/* 2. Add these routes to match the requested locations */}
+      <Route path="/admin/rooms" element={<ManageRooms />} />
+      <Route path="/admin/bookings" element={<ManageBookings />} />
+      <Route path="/admin/users" element={<ManageUsers />} />
     </Routes>
   );
 }
